afa9f3f4828bb2d66d409e237d651dbcd8f3881b
Wardan <mras@outlook.sa>
2021-05-03 22:49:59 +0800

    Fixed github

---
+++ myapp/middleware/auth.js
@@ -0,0 +1,14 @@
+
+
+module.exports = {
+    ensureAuth: function (req, res, next) {
+        if (req.session.user) {
+            next();
+          } else {
+            res.redirect("/login");
+          }
+  },
+
+  
+};
+
